29552140e55ee20b56a687c7c615e76dc26a4d75
Jan Quijano <jan.quijano@nutrien.com.au>
2022-10-25 15:55:07 +1100

    add option to pass aws credentials

--- cloudwatch/cloudwatch-bulk-add-retention-policy.py
+++ cloudwatch/cloudwatch-bulk-add-retention-policy.py
@@ -1,7 +1,15 @@
 import boto3
+import os
 
 
-logs = boto3.client('logs')
+days_retention = os.environ.get('RETENTION_IN_DAYS', 90)
+access_key = os.environ.get('ACCESS_KEY')
+secret_key = os.environ.get('SECRET_KEY')
+logs = boto3.client(
+    'logs',
+    aws_access_key_id = access_key,
+    aws_secret_access_key = secret_key,
+)
 
 
 response = logs.describe_log_groups()
@@ -22,5 +30,5 @@ for log_group in log_groups:
     if retention_in_days is None:
         response = logs.put_retention_policy(
             logGroupName = log_group_name,
-            retentionInDays = 90
+            retentionInDays = days_retention
         )
